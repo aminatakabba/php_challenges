c8b85f4df4ccef99aaea1946ce98ce687dc9874c
Aminata Kabba <aminata-kabba@hotmail.com>
2020-07-28 18:23:36 +0100

    HTML-PHP calculator

---
+++ HTML-PHP calculator/addition.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+// <?php print_r($_GET)?>
+<?="<h1>The result is: <h2>"?>
+<?=$_GET['add_first']+$_GET['add_second']?>
+<a href="index.php">Reset</a>
+</body>
+</html>
---
+++ HTML-PHP calculator/division.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+// <?php print_r($_GET)?>
+<?="<h1>The result is: <h2>"?>
+<?=$_GET['div_num']/$_GET['div_den']?>
+<a href="index.php">Reset</a>
+</body>
+</html>
